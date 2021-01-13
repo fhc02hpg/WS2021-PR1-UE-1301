9073c0abbc97cff7fa9ee79edb9305a6c9799790
fhc02hpg <noreply@github.com>
2021-01-13 20:03:21 +0100

    further solutions

--- src/org/campus02/emp/DemoEM.java
+++ src/org/campus02/emp/DemoEM.java
@@ -1,5 +1,8 @@
 package org.campus02.emp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DemoEM {
 
     public static void main(String[] args) {
@@ -14,6 +17,15 @@ public class DemoEM {
         em.addEmployee(new Employee(1005,"Keith",3750.00,"Sales"));
         em.addEmployee(new Employee(1006,"Peter",3000.00,"Education"));
         em.addEmployee(new Employee(1007,"Joane",4500.00,"Engineering"));
+
+        //1) employee list per department
+        //k: Finance, v: [Kim]
+        //k: Sales, v: [Jennifer,Keith]
+        //...
+        HashMap<String, ArrayList<Employee>> result1 = em.getAllEmployeesPerDepartment();
+        System.out.println(result1);
+
+
 
         //TODO: test the new methods here...
 
--- src/org/campus02/emp/EmployeeManager.java
+++ src/org/campus02/emp/EmployeeManager.java
@@ -1,6 +1,7 @@
 package org.campus02.emp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EmployeeManager {
 
@@ -30,5 +31,30 @@ public class EmployeeManager {
         return found;
 
     }
+
+    public HashMap<String, ArrayList<Employee>> getAllEmployeesPerDepartment() {
+
+        HashMap<String, ArrayList<Employee>> resultMap = new HashMap<>();
+
+        for(Employee e : employees) {
+            if(resultMap.containsKey(e.getDepartment())) {
+                //update the map value
+                ArrayList<Employee> list = resultMap.get(e.getDepartment());
+                list.add(e);
+                resultMap.put(e.getDepartment(),list);
+            } else {
+                //put dept with list and 1 employee
+                ArrayList<Employee> list = new ArrayList<>();
+                list.add(e);
+                resultMap.put(e.getDepartment(),list);
+            }
+        }
+
+        return resultMap;
+
+    }
+
+
+
 
 }
